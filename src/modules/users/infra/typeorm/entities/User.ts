7cccabd33be288b4af17765720e0dfa654c80276
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-11 20:40:04 -0300

    db: add user entity for typeorm

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { UserStatus } from "../../../enums/UserStatus";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  type: string;
+
+  @Column({ type: "enum", enum: UserStatus })
+  status: UserStatus;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
