63fb27e4bf238d5f485b7f5f2d40b4e874f43b49
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-11 20:23:55 -0300

    db: add users table migration

---
+++ src/shared/infra/typeorm/migrations/1620774794292-users-table.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class usersTable1620774794292 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "type",
+            type: "varchar",
+          },
+          {
+            name: "status",
+            type: "enum",
+            enum: ["ativo", "inativo"],
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
