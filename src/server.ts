17ade6b8fc682eaeb8a50b648f94f8238d8f24fc
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-11 21:31:06 -0300

    add routes file and first user module path

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
 import "./shared/infra/typeorm";
+import { router } from "./shared/infra/http/routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3333);
