23ccb47343c6d7ab3f5bc08a496daae2320fd835
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 20:02:08 -0300

    feat: add delete user route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 
 import { CreateUserController } from "@modules/users/useCases/createUser/CreateUserController";
+import { DeleteUserController } from "@modules/users/useCases/deleteUser/DeleteUserController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const deleteUserController = new DeleteUserController();
 
 usersRoutes.post("/", ensureAuthenticated, createUserController.handle);
+usersRoutes.delete("/:user_id", deleteUserController.handle);
 
 export { usersRoutes };
