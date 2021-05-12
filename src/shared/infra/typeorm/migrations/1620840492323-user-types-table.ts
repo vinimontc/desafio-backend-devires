e61dc591c983d50742605a54e47ed0fc4f21cbef
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 15:06:41 -0300

    db: add user types migration

---
+++ src/shared/infra/typeorm/migrations/1620840492323-user-types-table.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class userTypesTable1620840492323 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "user_types",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "title",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user_types");
+  }
+}
