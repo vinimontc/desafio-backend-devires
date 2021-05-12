d0eb4f67b3725dc78187236753b3ef59c7016d86
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 18:20:59 -0300

    test: add first create user use case test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,51 @@
+import { UserStatus } from "@modules/users/enums/UserStatus";
+import { UsersRepositoryInMemory } from "@modules/users/repositories/in-memory/UsersRepositoryInMemory";
+import { UserTypesRepositoryInMemory } from "@modules/users/repositories/in-memory/UserTypesRepositoryInMemory";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let userTypesRepositoryInMemory: UserTypesRepositoryInMemory;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    userTypesRepositoryInMemory = new UserTypesRepositoryInMemory();
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to create a new user with root requester", async () => {
+    const rootType = await userTypesRepositoryInMemory.create({
+      title: "root",
+      description: "Usuário com permissões completas na aplicação.",
+    });
+
+    const geralType = await userTypesRepositoryInMemory.create({
+      title: "geral",
+      description: "Usuário com permissões limitadas as suas informações.",
+    });
+
+    const rootUser = await usersRepositoryInMemory.create({
+      name: "Joseph Rogers",
+      email: "desa@waz.sd",
+      password: "123456",
+      type_id: rootType.id,
+      status: UserStatus.ATIVO,
+    });
+
+    rootUser.type = rootType;
+
+    const user = await createUserUseCase.execute({
+      name: "Clayton Carson",
+      email: "cagpapof@zaf.sv",
+      password: "123456",
+      type_id: geralType.id,
+      status: UserStatus.ATIVO,
+      request_user_id: rootUser.id,
+    });
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toEqual("Clayton Carson");
+  });
+});
