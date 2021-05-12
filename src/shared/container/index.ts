7d679a5d4cb0c97a6f312129305fca32acf8677e
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 16:50:30 -0300

    add user types repository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/users/infra/typeorm/repositories/UsersRepository";
+import { UserTypesRepository } from "@modules/users/infra/typeorm/repositories/UserTypesRepository";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { IUserTypesRepository } from "@modules/users/repositories/IUserTypesRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IUserTypesRepository>(
+  "UserTypesRepository",
+  UserTypesRepository
 );
