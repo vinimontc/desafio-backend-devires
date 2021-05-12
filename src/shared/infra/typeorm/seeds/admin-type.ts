e49c58320df1f573bd50e8738214dbe7dbb2f773
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 16:47:35 -0300

    add user types seeds

--- src/shared/infra/typeorm/seeds/admin-type.ts
+++ src/shared/infra/typeorm/seeds/admin-type.ts
@@ -3,11 +3,11 @@ import createConnection from "../index";
 async function create() {
   const connection = await createConnection("localhost");
 
-  const id = 1;
+  const id = 2;
 
   await connection.query(
     `INSERT INTO USER_TYPES(id, title, description, created_at)
-      values('${id}', 'root',  'Usuário com permissões completas na aplicação.', 'now()')`
+      values('${id}', 'admin',  'Usuário com permissão de criação e update de usuário.', 'now()')`
   );
 
   await connection.close();
--- src/shared/infra/typeorm/seeds/geral-type.ts
+++ src/shared/infra/typeorm/seeds/geral-type.ts
@@ -3,11 +3,11 @@ import createConnection from "../index";
 async function create() {
   const connection = await createConnection("localhost");
 
-  const id = 1;
+  const id = 3;
 
   await connection.query(
     `INSERT INTO USER_TYPES(id, title, description, created_at)
-      values('${id}', 'root',  'Usuário com permissões completas na aplicação.', 'now()')`
+      values('${id}', 'geral',  'Usuário comum, apenas possui permissão para edição dos seus próprios dados com excessão do tipo e status.', 'now()')`
   );
 
   await connection.close();
