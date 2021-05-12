371d7c4683b81df5568bfe293b6fa7e9f7c1a0c4
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-12 20:05:58 -0300

    add request user id to delete user controller

--- src/modules/users/useCases/deleteUser/DeleteUserController.ts
+++ src/modules/users/useCases/deleteUser/DeleteUserController.ts
@@ -6,10 +6,11 @@ import { DeleteUserUseCase } from "./DeleteUserUseCase";
 class DeleteUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { user_id } = request.params;
+    const { id: request_user_id } = request.user;
 
     const deleteUserUseCase = container.resolve(DeleteUserUseCase);
 
-    await deleteUserUseCase.execute({ user_id });
+    await deleteUserUseCase.execute({ request_user_id, user_id });
 
     return response.status(204).send();
   }
