ad3642af1763ab6c062f49c72307f2593584cb23
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-13 11:02:02 -0300

    feat: add show user profile route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from "@modules/users/useCases/createUser/CreateUserController";
 import { DeleteUserController } from "@modules/users/useCases/deleteUser/DeleteUserController";
 import { ListUserTypesController } from "@modules/users/useCases/listUserTypes/ListUserTypesController";
+import { ShowUserProfileController } from "@modules/users/useCases/showUserProfile/ShowUserProfileController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -11,6 +12,7 @@ const usersRoutes = Router();
 const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const listUserTypesController = new ListUserTypesController();
+const showUserProfileController = new ShowUserProfileController();
 
 usersRoutes.post("/", ensureAuthenticated, createUserController.handle);
 usersRoutes.delete(
@@ -19,5 +21,10 @@ usersRoutes.delete(
   deleteUserController.handle
 );
 usersRoutes.get("/types", ensureAuthenticated, listUserTypesController.handle);
+usersRoutes.get(
+  "/profile/:user_id",
+  ensureAuthenticated,
+  showUserProfileController.handle
+);
 
 export { usersRoutes };
