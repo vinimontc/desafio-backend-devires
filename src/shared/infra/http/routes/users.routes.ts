cbffa0d7d6fea9b373aa0923700bf9770c2a15d9
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-05-13 11:18:38 -0300

    feat: add update status route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "@modules/users/useCases/createUser/CreateU
 import { DeleteUserController } from "@modules/users/useCases/deleteUser/DeleteUserController";
 import { ListUserTypesController } from "@modules/users/useCases/listUserTypes/ListUserTypesController";
 import { ShowUserProfileController } from "@modules/users/useCases/showUserProfile/ShowUserProfileController";
+import { UpdateStatusController } from "@modules/users/useCases/updateStatus/UpdateStatusController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -13,6 +14,7 @@ const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const listUserTypesController = new ListUserTypesController();
 const showUserProfileController = new ShowUserProfileController();
+const updateStatusController = new UpdateStatusController();
 
 usersRoutes.post("/", ensureAuthenticated, createUserController.handle);
 usersRoutes.delete(
@@ -25,6 +27,11 @@ usersRoutes.get(
   "/profile/:user_id",
   ensureAuthenticated,
   showUserProfileController.handle
+);
+usersRoutes.patch(
+  "/status/:user_id",
+  ensureAuthenticated,
+  updateStatusController.handle
 );
 
 export { usersRoutes };
